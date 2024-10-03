0a40791f04bfc4869d469bbfd811834657ab503a
jusuf <jusuf.redzepi@ehb.be>
2024-10-03 13:29:10 +0200

    github profiel logo

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,3 +1,20 @@
 "use strict";
 
-alert("Draw your space invader here");
+let canvas = document.querySelector("canvas");
+canvas.width = innerWidth;
+canvas.Height = innerHeight;
+let context = canvas.getContext("2d"); 
+function drawsquare(x, y, ) {
+    context.beginPath();
+    context.moveTo (x, y);
+    context.lineTo(x + size, y);
+    context.lineTo(x + size, y + size)
+    context
+    context
+    context
+}
+
+context.fillStyle = '#000000'
+context.fillRect(0, 0, 300, 300);
+drawsquare(50, 50, 50);
+
